f332424b7f93c4de59e64a9680b195a8da0ea14e
Magnag <37120232+Matvey0094@users.noreply.github.com>
2026-01-20 13:19:27 +0300

    Update aliases.zsh

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,1 +1,16 @@
-t
+# ── fastfetch autostart ──
+[ -t 1 ] && command -v fastfetch >/dev/null 2>&1 && { clear; fastfetch; }
+
+command -v eza >/dev/null && alias ls='eza -lh --icons --group-directories-first --git --no-time' && alias la='eza -lah --icons --group-directories-first --git --time-style=long-iso'
+command -v bat >/dev/null && alias cat='bat -p'
+
+alias ..='cd ..'
+alias ...='cd ../..'
+
+
+
+# colors
+command -v vivid >/dev/null 2>&1 && export LS_COLORS="$(viv
+id generate zenburn)"
+export EZA_COLORS="da=38;5;205:hd=38;5;141:sn=38;5;110:uu=3
+8;5;250:gu=38;5;250"
